26fe58f74baa39cbe4b04aab2cab818c15181d10
dave-nagib <davidnagib101@gmail.com>
2023-04-20 15:51:01 +0200

    Graph constructor

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,24 +1,49 @@
 package Graph;
 
+import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Graph {
-    int size;
-    ArrayList<Edge>[] adjList;
+    ArrayList<ArrayList<Edge>> adjList;
 
-    public Graph(){
-        //TODO
+    public Graph(String filename){
+        try{
+            Scanner scan = new Scanner(new File(filename));
+            int v, e, start, dest, w;
+            v = scan.nextInt();
+            adjList = new ArrayList<>();
+            for(int i=0 ; i<v ; i++) adjList.add(new ArrayList<>());
+            e = scan.nextInt();
+            for (int i=0 ; i<e ; i++){
+                start = scan.nextInt();
+                dest = scan.nextInt();
+                w = scan.nextInt();
+                adjList.get(start).add(new Edge(dest, w));
+            }
+            scan.close();
+        }catch(Exception e){
+            System.out.println("nope");
+        }
     }
+
     void dijkestra(int source, int[] costs, int[] parents){
         //TODO
     }
 
     boolean bellmanFord(int source, int[] costs, int[] parents){
         //TODO
+        return false;
     }
 
     boolean floydWarshall(int[][] costs, int[][] parents){
         //TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph("example.txt");
+        System.out.println("hello");
     }
 }
