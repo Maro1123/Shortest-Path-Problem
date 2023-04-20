9c09747572a24f90a3c573dc68e0137e77c022cc
dave-nagib <davidnagib101@gmail.com>
2023-04-20 14:58:39 +0200

    Graph & Edge starts

--- src/Graph/Edge.java
+++ src/Graph/Edge.java
@@ -1,4 +1,10 @@
 package Graph;
 
 public class Edge {
+    public int to;
+    public int weight;
+    public Edge(int to, int weight){
+        this.to = to;
+        this.weight = weight;
+    }
 }
--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,4 +1,24 @@
 package Graph;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Graph {
+    int size;
+    ArrayList<Edge>[] adjList;
+
+    public Graph(){
+        //TODO
+    }
+    void dijkestra(int source, int[] costs, int[] parents){
+        //TODO
+    }
+
+    boolean bellmanFord(int source, int[] costs, int[] parents){
+        //TODO
+    }
+
+    boolean floydWarshall(int[][] costs, int[][] parents){
+        //TODO
+    }
 }
