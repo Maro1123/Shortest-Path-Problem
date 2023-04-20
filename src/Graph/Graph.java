bc6e6bb0dabf04de1365facee5de8fd6a57b1969
Random <ehabflash833@gmail.com>
2023-04-20 17:27:51 +0200

    floyd

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,13 +1,14 @@
 package Graph;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Graph {
     ArrayList<Edge>[] adjList;
     int size;
+    final int MAX = Integer.MAX_VALUE;
 
     public Graph(String filename){
         try{
@@ -41,21 +42,30 @@ public class Graph {
         return false;
     }
 
-
+    boolean floydWarshall() {
+        int[][] costs = new int[size][size], parents = new int[size][size];
+        for(int[] arr : costs)
+            Arrays.fill(arr, MAX);
+        for(int i = 0; i < size; i++) {
+            costs[i][i] = 0;
+            for(Edge edge : adjList[i])
+                costs[i][edge.to] = edge.weight;
+        }
+        return floydWarshall(costs, parents);
+    }
 
     boolean floydWarshall(int[][] costs, int[][] parents) {
-        int n = costs.length;
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (costs[i][j] > costs[i][k] + costs[k][j]) {
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (costs[i][k] != MAX && costs[k][j] != MAX && costs[i][k] + costs[k][j] < costs[i][j]) {
                         costs[i][j] = costs[i][k] + costs[k][j];
                         parents[i][j] = k;
                     }
                 }
             }
         }
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < size; i++)
             if (costs[i][i] < 0)
                 return false;
         return true;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Graph.Graph;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Graph g = new Graph("example.txt");
+
     }
 }
