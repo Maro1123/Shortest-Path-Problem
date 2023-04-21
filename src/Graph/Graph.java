a1bf870451c51d20a13594dfa3e46cb8d58c8cef
EhabYasser25 <97472134+EhabYasser25@users.noreply.github.com>
2023-04-21 03:52:50 +0200

    Update Graph.java

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -101,6 +101,7 @@ public class Graph {
     }
 
     private void floyd_init(int[][] costs, int[][] parents) {
+        
         for(int[] arr : costs)
             Arrays.fill(arr, Integer.MAX_VALUE);
         for(int i = 0; i < size; i++) {
